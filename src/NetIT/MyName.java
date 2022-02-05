8a82397f1e63cb82c9d53bae1876c92ede982c98
&StacyKo <stanislava.dimitrowa@gmail.com>
2022-02-06 00:03:55 +0200

    secondHomeworkTask1

--- src/NetIT/MyName.java
+++ src/NetIT/MyName.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MyName {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String myName = scan.nextLine ();
+        String myName = scan.nextLine();
         System.out.printf("My name is %s", myName);
 
     }
