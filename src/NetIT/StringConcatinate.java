5dfd44a4f4eef2793cea6c8b0db03177eff62ca5
&StacyKo <stanislava.dimitrowa@gmail.com>
2022-02-06 00:52:24 +0200

    secondHomeworkTask3

---
+++ src/NetIT/StringConcatinate.java
@@ -0,0 +1,21 @@
+package NetIT;
+
+import java.util.Scanner;
+
+public class StringConcatinate {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        String text1 = scan.nextLine();
+        String text2 = scan.nextLine();
+        System.out.println(text1 + " " + text2);
+
+        String commonText = text1 + text2;
+
+
+
+
+
+
+    }
+
+}
