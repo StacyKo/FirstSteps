0ee92a1c07a3afd47e0ba73a4ae909585fbf2a6f
&StacyKo <stanislava.dimitrowa@gmail.com>
2022-02-06 00:00:45 +0200

    secondHomeworkTask1

---
+++ src/NetIT/MyName.java
@@ -0,0 +1,12 @@
+package NetIT;
+
+import java.util.Scanner;
+
+public class MyName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String myName = scan.nextLine ();
+        System.out.printf("My name is %s", myName);
+
+    }
+}
