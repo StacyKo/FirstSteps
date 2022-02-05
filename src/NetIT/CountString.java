b830104f0770b52d16528496e071c217f6036631
&StacyKo <stanislava.dimitrowa@gmail.com>
2022-02-06 00:58:27 +0200

    secondHomeworkTask4

---
+++ src/NetIT/CountString.java
@@ -0,0 +1,17 @@
+package NetIT;
+
+import java.util.Scanner;
+
+public class CountString {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        String enterText = scan.nextLine();
+        System.out.println(enterText.lenght());
+
+
+
+
+    }
+}
